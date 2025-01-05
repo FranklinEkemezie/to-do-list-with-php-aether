d33a79fde5ff43795206a5159b5bf834429fb56f
Shadow Walker <franklynpeter2006@gmail.com>
2025-01-05 22:09:06 +0100

    update Error Controller to handle additional errors

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -9,39 +9,115 @@ use FranklinEkemezie\PHPAether\Core\Response;
 abstract class ErrorController extends BaseController
 {
 
-    public static function  notFound(): Response
+    // 4xx Client Errors
+
+    /**
+     * Send a 400 Bad Request Response.
+     * The client sent an invalid request (e.g., missing/invalid parameters).
+     * @param mixed $errorMsg
+     * @param mixed $errors
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function badRequest(?string $errorMsg=null, $errors=[]): Response
     {
-        // TODO
-        echo 'Not Found <br/>';
-        http_response_code(404);
-        return new Response;
+        return new Response(200, body: json_encode([
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'Invalid request',
+            'errors'    => $errors
+        ]));
     }
 
-    public static function methodNotAllowed(array $allowedMethods): Response
+    /**
+     * Send a 401 Unauthorised response
+     * Authentication failed or token is missing/invalid
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function unauthorised(?string $errorMsg=null): Response
     {
-        http_response_code(405);
 
-        echo 'Method not allowed <br/>';
-        return new Response;
+        return new Response(401, body: json_encode([
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'Authentication required or invalid credentials'
+        ]));
     }
 
-    public static function internalServerError(string $errorMsg): Response
+    /**
+     * Send a 403 Forbidden response
+     * User does not have permissiont to access the resource
+     * @param mixed $errorMsg
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function forbidden(?string $errorMsg=null): Response
     {
-        // TODO
-        http_response_code(500);
-
-        echo "$errorMsg <br/>";
-        return new Response;
+        return new Response(403, body: json_encode([
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'You do not have permission to perform this action'
+        ]));
     }
 
-    public static function unauthorised(): Response
+    /**
+     * Send a 404 Not Found response
+     * Resource not found
+     * @param mixed $errorMsg
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function  notFound(?string $errorMsg=null): Response
     {
+        return new Response(404, body: json_encode([
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'The requested resource could not be found'
+        ]));
+    }
 
-        // TODO
-        http_response_code(401);
+    /**
+     * Send a 405 Method Not Allowed Response
+     * The resource is found but the request method is not allowed.
+     * @param mixed $errorMsg
+     * @param array $allowedMethods
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function methodNotAllowed(?string $errorMsg=null, array $allowedMethods=[]): Response
+    {
+        $allowedMethods = array_map(fn(string $method) => strtoupper($method), $allowedMethods);
+        $responseBody   = [
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'Method not allowed',
+            'allowed_methods' => $allowedMethods
+        ];
 
-        echo "Authentication required <br/>";
-        return new Response;
+        return (new Response(405, body: json_encode($responseBody)))
+            ->setHeader('Allow', implode(', ', $allowedMethods))
+        ;
+    }
+
+    // 5xx Server Errors
+
+    /**
+     * Send a 500 Internal Server Error
+     * A general error occurred on the server
+     * @param mixed $errorMsg
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function internalServerError(?string $errorMsg=null): Response
+    {
+        return new Response(500, body: json_encode([
+            'status'    => 'error',
+            'error'     => $errorMsg ?? 'An unexpected error occurred on the server'
+        ]));
+    }
+
+    /**
+     * Send a 503 Service Unavailable Response.
+     * Server is down or overloaded.
+     * @param mixed $errorMsg
+     * @return \FranklinEkemezie\PHPAether\Core\Response
+     */
+    public static function serviceUnavailable(?string $errorMsg=null): Response
+    {
+        return new Response(503, body: json_encode([
+            'status'    => 'error',
+            'message'   => $errorMsg ?? 'The service is temporarily unavailable'
+        ]));
     }
 
 }
