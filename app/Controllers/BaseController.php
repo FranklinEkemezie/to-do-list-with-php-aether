16ef6fabfc05c92e506fa79ff37da8dc20ed8b8f
Shadow Walker <franklynpeter2006@gmail.com>
2025-01-06 10:19:57 +0100

    add methods to handle 2xx success response

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace FranklinEkemezie\PHPAether\Controllers;
 
-use FranklinEkemezie\PHPAether\Core\Database;
-use FranklinEkemezie\PHPAether\Core\Request;
+use FranklinEkemezie\PHPAether\Core\{
+    Database,
+    Request,
+    Response
+};
+
 
 abstract class BaseController
 {
@@ -16,6 +20,55 @@ abstract class BaseController
     )
     {
         
+    }
+
+    /**
+     * Send a 200 OK response
+
+     * Use case: Request succeeded, and the response contains the requested data
+     * @param mixed $message
+     * @param array $payload
+     * @return Response
+     */
+    protected static function respondOK(?string $message=null, array $payload): Response
+    {
+        return new Response(body: json_encode([
+            'status'=> 'success',
+            'data'  => [
+                'message'   => $message ?? 'Request processed successfully',
+                'payload'   => $payload
+            ]
+        ]));
+    }
+
+    /**
+     * Send a 201 Create Response
+
+     * Use case: Resource was successfully created
+     * @param mixed $message
+     * @param string|int $resourceId
+     * @return Response
+     */
+    protected static function respondCreated(?string $message=null, string $resourceNameId='id', string|int $resourceId): Response
+    {
+        return new Response(201, body: json_encode([
+            'status'    => 'success',
+            'data'      => [
+                'message'       => $message ?? 'Resource created successfully',
+                $resourceNameId => $resourceId
+            ]
+        ]));
+    }
+
+    /**
+     * Send a 204 No Content
+
+     * Use case: Request processed successfully, but no data is returned
+     * @return Response
+     */
+    protected static function responseNoContent(): Response
+    {
+        return new Response(204);
     }
 
 }
