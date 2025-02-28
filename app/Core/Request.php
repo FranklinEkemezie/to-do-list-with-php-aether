5cb466986de369225da17be278254c1719966114
Shadow Walker <franklynpeter2006@gmail.com>
2025-02-28 21:34:51 +0100

    add Request class

--- app/Core/Request.php
+++ app/Core/Request.php
@@ -2,90 +2,32 @@
 
 declare(strict_types=1);
 
-namespace FranklinEkemezie\PHPAether\Core;
+namespace PHPAether\Core;
 
-use FranklinEkemezie\PHPAether\Exceptions\UndefinedException;
-use FranklinEkemezie\PHPAether\Utils\Dictionary;
 
-/**
- * Request class
- * 
- * Encapsulates and manages incoming HTTP request data.
- * 
- * @package FranklinEkemezie\PHPAether\Core
- */
+use Exception;
+
 class Request
 {
-    /**
-     * The request path
-     * @var string
-     */
-    private string $path;
+
+    public readonly string $routePath;
+    public readonly string $method;
 
     /**
-     * The request method
-     * @var string
+     * @throws Exception
      */
-    private string $method;
+    public function __construct(
 
-    /**
-     * The authentication token
-     * @var string
-     */
-    private ?string $authToken;
-
-    private Dictionary $GET;
-    private Dictionary $POST;
-    private Dictionary $SESSION;
-    private Dictionary $COOKIES;
-
-    public function __construct()
+    )
     {
-        $this->path     = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $this->method   = strtoupper($_SERVER['REQUEST_METHOD']);
-
-        $this->authToken= self::getAuthToken();
-
-        $this->GET      = new Dictionary($_GET);
-        $this->POST     = new Dictionary($_POST);
-        $this->SESSION  = new Dictionary($_SESSION);
-        $this->COOKIES  = new Dictionary($_COOKIE);
-    }
-
-    private static function getAuthToken(): ?string
-    {
-
-        $authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? null;
-        if ($authHeader === null) return null;
-
-        $authToken  = str_replace('Bearer ', '', $authHeader);
-        if ($authToken === '') return null;
-
-        return $authToken;
-    }
-
-    public function isGet(): bool
-    {
-        return $this->method === 'GET';
-    }
-
-    public function isPost(): bool
-    {
-        return $this->method === 'POST';
-    }
-
-    public function redirect(string $newUrl): void
-    {
-        header("Location: $newUrl");
-        exit;
-    }
-
-    public function __get(string $name): mixed
-    {
-        if (property_exists($this, $name)) {
-            return $this->$name;
+        $routeInfo = parse_url($_SERVER['REQUEST_URI']);
+        if ($routeInfo === false) {
+            throw new Exception('Could not parse request url. URL may be malformed');
         }
 
-        throw new UndefinedException('Undefined property: ' . __CLASS__ . '::$' . $name);
+        $this->routePath = $routeInfo['path'];
+        $this->method = $_SERVER['REQUEST_METHOD'];
     }
+
+
 }
---
+++ tests/Unit/Core/RequestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHPAether\Tests\Unit\Core;
+
+use PHPAether\Core\Request;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+
+    public static function requestTestCases(): array
+    {
+        return [
+            ['/', 'GET', '/'],
+            ['/login?r_url=/user/dashboard', 'POST', '/login'],
+            ['/auth/otp/verify', 'GET', '/auth/otp/verify'],
+            ['/user/profile', 'PUT', '/user/profile']
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('requestTestCases')]
+    public function it_gets_route_path(
+        string $requestUri,
+        string $requestMethod,
+        string $expectedRoutePath
+    )
+    {
+        $_SERVER['REQUEST_METHOD'] = $requestMethod;
+        $_SERVER['REQUEST_URI'] = $requestUri;
+
+        $request = new Request();
+
+        $this->assertSame($expectedRoutePath, $request->routePath);
+    }
+
+}
