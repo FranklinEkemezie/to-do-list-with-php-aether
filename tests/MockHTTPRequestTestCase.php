37172a454a305b97667650f44747af0b07e70920
Shadow Walker <franklynpeter2006@gmail.com>
2025-03-08 13:32:35 +0100

    add test cases for different HTTP requests

--- tests/MockHTTPRequestTestCase.php
+++ tests/MockHTTPRequestTestCase.php
@@ -7,59 +7,119 @@ use PHPUnit\Framework\TestCase;
 class MockHTTPRequestTestCase extends TestCase
 {
 
-    protected array $routes;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Routes
-        $this->routes = [
-            '/' => [
-                'controller'    => 'Home',
-                'action'        => 'index',
-                'methods'       => ['GET']
+    protected const TEST_ROUTES = [
+        '/' => [
+            'controller'    => 'Home',
+            'action'        => 'index',
+            'methods'       => ['GET']
+        ],
+        '/login' => [
+            'controller'    => 'Auth',
+            'GET'   => [
+                'action'    => 'loginView'
             ],
-            '/login' => [
-                'controller'    => 'Auth',
+            'POST'  => [
+                'action'    => 'login'
+            ]
+        ],
+        '/register' => [
+            'controller'    => 'Auth',
+            'action'        => 'register',
+            'methods'       => ['POST']
+        ],
+        '/user/profile/update' => [
+            'controller' => 'User',
+            'GET'   => [
+                'action' => 'userProfileView'
+            ],
+            'PUT'  => [
+                'action' => 'updateUserProfile'
+            ]
+        ],
+        '/user/dashboard' => [
+            'controller'    => 'User',
+            'action'        => 'userDashboardView'
+        ]
+    ];
 
-                'GET'   => [
-                    'action'    => 'loginView'
-                ],
-                'POST'  => [
-                    'action'    => 'login'
-                ]
+
+    public static function setUpHTTPRequestTest(
+        string $route, string $method
+    ): void
+    {
+        $_SERVER['REQUEST_URI'] = $route;
+        $_SERVER['REQUEST_METHOD'] = $method;
+    }
+
+    public static function httpRequestDataProvider(): \Generator
+    {
+
+        $dataProviders = [
+            'GET'   => [self::class, 'httpGETRequestDataProvider'],
+            'POST'  => [self::class, 'httpPOSTRequestDataProvider'],
+            'PUT'   => [self::class, 'httPUTRequestDataProvider'],
+        ];
+
+        foreach ($dataProviders as $requestMethod => $dataProvider) {
+
+            $testCases = $dataProvider();
+            foreach ($testCases as $testCase) {
+                $route = $testCase[0];
+                $testCase = self::buildHTTPRequestDataProviderTestCase(
+                    $requestMethod, ...$testCase
+                );
+
+                yield "HTTP Request Test Case [$requestMethod] $route" => $testCase;
+            }
+        }
+    }
+
+    public static function buildHTTPRequestDataProviderTestCase(
+        string $requestMethod,
+        string $route,
+        string $expectedRoute,
+        array $expectedAction,
+        string $expectedResponse
+    ): array
+    {
+        return [
+            'route'     => $route,
+            'method'    => strtoupper($requestMethod),
+            // expected values
+            'expected'  => [
+                'route'     => $expectedRoute,
+                'action'    => $expectedAction,
+                'response'  => $expectedResponse
             ]
         ];
-
-        // Set up SERVER variables
-        $_SERVER['REQUEST_URI'] = '/';
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-
     }
 
-    public static function httpGETRequestTestCases(): array
+    public static function httpGETRequestDataProvider(): array
     {
-        return [
-
+        return  [
+            // [$route, $_route, $_action, $_response] (_ means "expected")
+            ['/', '/', ['Home', 'index'], ''],
+            ['/login?r_url=/user/dashboard', '/login', ['Auth', 'loginView'], ''],
+            ['/user/dashboard', '/user/dashboard', ['User', 'userDashboardView'], ''],
+            ['/leaderboard?league=ruby', '/leaderboard', ['Error', 'notFound'], '']
         ];
     }
-    public static function httpPOSTRequestTestCases(): array
+    public static function httpPOSTRequestDataProvider(): array
     {
         return [
-
+            // [$route, $_route, $_action, $_response] (_ means "expected")
+            ['/register', '/register', ['Auth', 'register'], ''],
+            ['/login', '/login', ['Auth', 'login'], ''],
+            ['/cart', '/cart', ['Error', 'notFound'], '']
         ];
     }
 
-    protected function tearDown(): void
+    public static function httPUTRequestDataProvider(): array
     {
-        parent::tearDown();
-
-        // Clear Server variables
-        unset(
-            $_SERVER['REQUEST_URI'],
-            $_SERVER['REQUEST_METHOD']
-        );
-
+        return [
+            // [$route, $_route, $_action, $_response] (_ means "expected")
+            ['/user/profile/update', '/user/profile/update', ['User', 'updateUserProfile'], '']
+        ];
     }
+
 }
