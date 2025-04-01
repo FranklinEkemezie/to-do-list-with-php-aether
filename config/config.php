f8854884407ecf288eece29dd440aab1feb368b6
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-01 05:08:59 +0100

    add app config file

---
+++ config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . "/constants.php";
+
+return [
+
+    /**
+     * Database Configuration
+     */
+    'DB'    => [
+
+    ],
+
+    /*
+     * Authentication Configuration
+     */
+    'AUTH'  => [
+
+    ],
+
+    /*
+     * View Configuration
+     */
+    'VIEW'  => [
+        'DEFAULT_VIEW_DIR'  => ROOT_DIR . "/resources/views"
+    ]
+];
