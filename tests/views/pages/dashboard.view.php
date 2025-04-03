9ba5ccd94674101111b8969be75e7d4aaf540da5
FranklinEkemezie <franklynpeter2006@gmail.com>
2025-04-03 06:45:20 +0100

    add test views for variable and loops parsing

--- tests/views/pages/dashboard.view.php
+++ tests/views/pages/dashboard.view.php
@@ -1,1 +1,15 @@
-Welcome, { name } ({ email })!
+Welcome, {{ name }}!
+Email: {{ email }}
+ID: USER_{{ user.id }}
+
+@{
+    for(invoice of invoices) {
+        <p>Invoice ID: {{ invoice.id }}</p>
+    }
+}}
+
+@{
+    for(key: value of bio_info) {
+        {{ key }}: {{ 
+    }
+}}
