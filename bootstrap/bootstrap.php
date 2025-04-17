e945de82894e2287118c76a2b6cc2d91a2c2331c
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-17 23:45:36 +0100

    add app bootstrap files

---
+++ bootstrap/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once dirname(__DIR__) . "/bootstrap/constants.php";
+
+// Set up config
+\PHPAether\Utils\Config::load(ROOT_DIR . "/config/config.php");
---
+++ bootstrap/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ROOT_DIR', dirname(__DIR__));
+
+// Define commonly used directories
+const APP_DIR           = ROOT_DIR . '/app';
+const CONFIG_DIR        = ROOT_DIR . '/config';
