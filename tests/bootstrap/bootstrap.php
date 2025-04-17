5c4d889272150d71f5a3281926f196f3cc0b0f6a
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-17 23:33:35 +0100

    add test bootstrap files

---
+++ tests/bootstrap/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once dirname(__DIR__) . "/bootstrap/constants.php";
+
+// Set up config
+\PHPAether\Utils\Config::load(TESTS_DIR . "/config/config.php");
---
+++ tests/bootstrap/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT_DIR', dirname(__DIR__, 2));
+
+// Define commonly used directories
+const APP_DIR           = ROOT_DIR . '/app';
+const TESTS_DIR         = ROOT_DIR . '/tests';
+const CONFIG_DIR        = ROOT_DIR . '/config';
