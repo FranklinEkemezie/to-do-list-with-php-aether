e48a163dff77763bd8f210d65a1110236589a0ea
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-20 16:24:22 +0100

    add createMockRequest method to mock a Request object instance

--- tests/MockHTTPRequestTestCase.php
+++ tests/MockHTTPRequestTestCase.php
@@ -2,64 +2,45 @@
 
 namespace PHPAether\Tests;
 
+use PHPAether\Core\App;
+use PHPAether\Core\HTTP\Request;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class MockHTTPRequestTestCase extends TestCase
 {
+    protected static App $APP;
 
-    protected array $routes;
-
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        parent::setUp();
+        parent::setUpBeforeClass();
 
-        // Routes
-        $this->routes = [
-            '/' => [
-                'controller'    => 'Home',
-                'action'        => 'index',
-                'methods'       => ['GET']
-            ],
-            '/login' => [
-                'controller'    => 'Auth',
-
-                'GET'   => [
-                    'action'    => 'loginView'
-                ],
-                'POST'  => [
-                    'action'    => 'login'
-                ]
-            ]
-        ];
-
-        // Set up SERVER variables
-        $_SERVER['REQUEST_URI'] = '/';
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-
+        // Bootstrap application
+        static::$APP = require_once __DIR__ . '/bootstrap/bootstrap.php';
     }
 
-    public static function httpGETRequestTestCases(): array
+    public static function mockHTTPRequestTestCases(): \Generator
     {
-        return [
+        $testUrls = require TESTS_DIR . '/urls/urls.php';
+        foreach ($testUrls as $requestMethod => $testUrlTestCases) {
 
-        ];
+            foreach ($testUrlTestCases as $testUrlTestCase) {
+                $testUrlTestCase['method'] = $requestMethod;
+
+                $url = $testUrlTestCase['url'];
+                yield "[$requestMethod] $url" => $testUrlTestCase;
+            }
+        }
     }
-    public static function httpPOSTRequestTestCases(): array
+
+    public function createMockRequest(string $url, string $method): Request&MockObject
     {
-        return [
+        // Prepare server variables
+        $_SERVER['REQUEST_URI'] = $url;
+        $_SERVER['REQUEST_METHOD'] = $method;
 
-        ];
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        // Clear Server variables
-        unset(
-            $_SERVER['REQUEST_URI'],
-            $_SERVER['REQUEST_METHOD']
-        );
-
+        return $this->getMockBuilder(Request::class)
+            ->setConstructorArgs([$_SERVER])
+            ->getMock();
     }
 }
