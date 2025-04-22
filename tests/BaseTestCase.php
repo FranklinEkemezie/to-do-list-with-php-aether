d1fc41e7b2d9b772673d07b41f9fd5abcef58440
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 07:29:46 +0100

    load config, bootstrap application in BaseTestCase

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace PHPAether\Tests;
 
+require_once __DIR__ . '/bootstrap/constants.php';
+
+use PHPAether\Core\App;
 use PHPAether\Utils\Config;
 use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
 {
 
+    protected static App $APP;
+
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
 
-        require_once __DIR__ . "/../config/constants.php";
+        // bootstrap application
+        static::$APP = require TESTS_DIR . '/bootstrap/bootstrap.php';
 
-        Config::setUp(TESTS_DIR . "/config/config.php");
+        // load config
+        Config::load(TESTS_DIR . '/config/config.php');
     }
 }
