956b1f5649343955fffb54f8389180a48d1b78d7
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 02:59:24 +0100

    add test bootstrap file to bootstrap application

--- tests/bootstrap/bootstrap.php
+++ tests/bootstrap/bootstrap.php
@@ -1,6 +1,18 @@
 <?php
 
-require_once dirname(__DIR__) . "/bootstrap/constants.php";
+require_once dirname(__DIR__) . '/bootstrap/constants.php';
+require_once ROOT_DIR . '/vendor/autoload.php';
 
 // Set up config
-\PHPAether\Utils\Config::load(TESTS_DIR . "/config/config.php");
+\PHPAether\Utils\Config::load(TESTS_DIR . '/config/config.php');
+
+// Instantiate dependencies
+$router = (new \PHPAether\Core\HTTP\Router())
+    ->registerWebRoutesFromFile(TESTS_DIR . '/config/routes/web.php')
+    ->registerApiRoutesFromFile(TESTS_DIR . '/config/routes/api.php')
+    ->registerCliRoutesFromFile(TESTS_DIR . '/config/routes/cli.php')
+;
+
+$database = 'Database';
+
+return new \PHPAether\Core\App($router, $database);
