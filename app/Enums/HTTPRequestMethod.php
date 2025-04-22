23234212024c9a2860a0ba28d2e12d62e3074939
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 02:50:43 +0100

    update HTTPRequestMethod to define supported HTTP request methods

--- app/Enums/HTTPRequestMethod.php
+++ app/Enums/HTTPRequestMethod.php
@@ -10,4 +10,16 @@ enum HTTPRequestMethod
     case PUT;
     case PATCH;
     case DELETE;
+
+    public static function tryFrom(string $value): HTTPRequestMethod
+    {
+        return match ($value) {
+            'GET'   => HTTPRequestMethod::GET,
+            'POST'  => HTTPRequestMethod::POST,
+            'PUT'   => HTTPRequestMethod::PUT,
+            'PATCH' => HTTPRequestMethod::PATCH,
+            'DELETE'=> HTTPRequestMethod::DELETE,
+            default => throw new \InvalidArgumentException("Invalid HTTP Request Method: $value")
+        };
+    }
 }
