6b8ff4e9abb2b620ca5d5f4134a617a23caf2e59
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 03:32:03 +0100

    update Request class to add support for HTTPRequestMethod and RequestType enums

--- app/Core/HTTP/Request.php
+++ app/Core/HTTP/Request.php
@@ -2,31 +2,45 @@
 
 declare(strict_types=1);
 
-namespace PHPAether\Core;
+namespace PHPAether\Core\HTTP;
 
 
 use Exception;
+use PHPAether\Enums\HTTPRequestMethod;
+use PHPAether\Enums\RequestType;
 
 class Request
 {
 
-    public readonly string $routePath;
-    public readonly string $method;
+    public readonly string $path;
+    public readonly HTTPRequestMethod $method;
+    public readonly RequestType $type;
 
     /**
      * @throws Exception
      */
-    public function __construct(
-
-    )
+    public function __construct(?array $serverVariables=null, RequestType $requestType=RequestType::WEB)
     {
-        $routeInfo = parse_url($_SERVER['REQUEST_URI']);
+        $serverVariables ??= $_SERVER;
+
+        $requestUri = $serverVariables['REQUEST_URI'] ?? null;
+        if (! $requestUri) {
+            throw new \InvalidArgumentException("Missing required key 'REQUEST_URI' in \$serverVariables");
+        }
+
+        $requestMethod = $serverVariables['REQUEST_METHOD'] ?? null;
+        if (! $requestMethod) {
+            throw new \InvalidArgumentException("Missing required key 'REQUEST_METHOD' in \$serverVariables");
+        }
+
+        $routeInfo = parse_url($requestUri);
         if ($routeInfo === false) {
             throw new Exception('Could not parse request url. URL may be malformed');
         }
 
-        $this->routePath = $routeInfo['path'];
-        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->path     = (string) $routeInfo['path'];
+        $this->method   = HTTPRequestMethod::tryFrom(strtoupper($requestMethod));
+        $this->type     = $requestType;
     }
 
 
