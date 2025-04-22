15427d2cb381f04c3b8c891ab6c58e8965fa431f
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 02:44:56 +0100

    update data provider configuration for GET urls/endpoints

--- tests/urls/GET.php
+++ tests/urls/GET.php
@@ -1,10 +1,39 @@
 <?php
 
+// List of test routes to simulate HTTP GET request
+
 return [
     [
-        'url'       => '/users?page=2&sort=name',
+        'url'       => '/tests/home',
         'expected'  => [
-            'action'    => ['Users', 'getUsers']
+            'router'    => [
+                'route' => '/tests/home'
+            ]
+        ]
+    ],
+    [
+        'url'       => '/tests/books?page=2&sort=name',
+        'expected'  => [
+            'request'   => [
+                'query' => [
+                    'page'  => '2',
+                    'sort'  => 'name'
+                ]
+            ],
+            'router'    => [
+                'route' => '/tests/books'
+            ]
+        ]
+    ],
+    [
+        'url'       => '/tests/books/2',
+        'expected'  => [
+            'router'    => [
+                'route' => '/tests/books/:id',
+                'params'    => [
+                    'id'    => 2
+                ]
+            ]
         ]
     ]
 ];
