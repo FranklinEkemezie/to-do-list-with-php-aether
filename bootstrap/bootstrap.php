f61402c6e0d4f087f94c773c40388042f1d47f53
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 03:00:03 +0100

    add bootstrapp file to bootstrap application

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,6 +1,17 @@
 <?php
 
-require_once dirname(__DIR__) . "/bootstrap/constants.php";
+require_once dirname(__DIR__) . '/bootstrap/constants.php';
+require_once ROOT_DIR . '/vendor/autoload.php';
 
 // Set up config
-\PHPAether\Utils\Config::load(ROOT_DIR . "/config/config.php");
+\PHPAether\Utils\Config::load(ROOT_DIR . '/config/config.php');
+
+// Instantiate dependencies
+$router = (new \PHPAether\Core\HTTP\Router())
+    ->registerWebRoutesFromFile(ROOT_DIR . '/tests/config/routes/web.php')
+    ->registerApiRoutesFromFile(ROOT_DIR . '/config/routes/api.php')
+    ->registerCliRoutesFromFile(ROOT_DIR . '/config/routes/cli.php');
+
+$database = 'Database';
+
+return new \PHPAether\Core\App($router, $database);
