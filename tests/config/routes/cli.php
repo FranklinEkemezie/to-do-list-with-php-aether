8e4c926f89be69bfb7731ac6f2ac7e705d042591
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-22 02:49:29 +0100

    update test routes definitions

--- tests/config/routes/cli.php
+++ tests/config/routes/cli.php
@@ -1,6 +1,8 @@
 <?php
 
-return (function (\PHPAether\Core\HTTP\Router $router) {
+use PHPAether\Core\HTTP\Router;
+
+return (function (Router $router) {
 
     $router->get('/', fn() => '');
     $router->delete('/books/:id/delete', fn() => '');
