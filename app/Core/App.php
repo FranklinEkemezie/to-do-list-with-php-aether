cd7957ab45452e61c15db1f0a9f08f391770d8b4
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-25 11:13:12 +0100

    pass request to request handler action; handle router exceptions

--- app/Core/App.php
+++ app/Core/App.php
@@ -4,6 +4,9 @@ namespace PHPAether\Core;
 
 use PHPAether\Core\HTTP\Request;
 use PHPAether\Core\HTTP\Router;
+use PHPAether\Exceptions\Exception;
+use PHPAether\Exceptions\RouterExceptions\MethodNotAllowedException;
+use PHPAether\Exceptions\RouterExceptions\RouteNotFoundException;
 use PHPAether\Exceptions\RouterExceptions\RouterException;
 use PHPAether\Utils\Config;
 
@@ -18,16 +21,41 @@ class App
     }
 
     /**
-     * @throws RouterException
+     * @throws Exception
      */
     public function run(Request $request): string
     {
-        // Route requests
-        [
-            'middlewares'   => $middlewares,
-            'action'        => $action,
-            'params'        => $params
-        ] = $this->router->route($request);
+
+        try {
+
+            // Route requests
+            [
+                'middlewares'   => $middlewares,
+                'action'        => $action,
+                'params'        => $params
+            ] = $this->router->route($request);
+        } catch (MethodNotAllowedException $exception) {
+            // TODO: Handle method not allowed exception
+
+            return "Method Not Allowed\n";
+        } catch (RouteNotFoundException $exception) {
+            // TODO: Handle route not found exception
+
+            return "Route Not Found Exception";
+        } catch (RouterException $exception) {
+            // TODO: Handle general router exception
+
+            return "An error occurred while routing";
+        } catch (Exception $exception) {
+            // TODO: Handle any exception
+
+            return "An error occurred";
+        }
+
+        // Ensure the action is callable
+        if (! is_callable($action)) {
+            throw new Exception("Invalid request handler. Route action is not callable");
+        }
 
         // Resolve middlewares
         foreach ($middlewares as $middleware) {
@@ -36,6 +64,6 @@ class App
             $middleware->resolve();
         }
 
-        return $action($request, ...$params);
+        return $action($request);
     }
 }
