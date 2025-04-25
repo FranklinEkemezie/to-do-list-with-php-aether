f00052b0c50ddcaf192838d6d922498839e95e59
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-25 11:02:51 +0100

    update tests URLs loader to read the new tests request JSON files

--- tests/urls/urls.php
+++ tests/urls/urls.php
@@ -1,9 +1,32 @@
 <?php
 
+/**
+ * @throws Exception
+ */
+$getUrls = function (string $method): array {
+    $jsonFile = __DIR__ . "/" . strtoupper($method) . ".json";
+    if (! file_exists($jsonFile)) {
+        throw new \Exception("JSON file: $jsonFile not found");
+    }
+
+    $urlsJson = file_get_contents($jsonFile);
+    if (! $urlsJson) {
+        throw new \Exception("Could not get the content of the JSON file");
+    }
+
+    $urls = json_decode($urlsJson, true);
+    if (! is_array($urls)) {
+        print_r($urls);
+        throw new \Exception("Invalid JSON or URL configuration");
+    }
+
+    return $urls;
+};
+
 return [
-    'GET'   => require __DIR__ . '/GET.php',
-    'POST'  => require __DIR__ . '/POST.php',
-    'PUT'   => require __DIR__ . '/PUT.php',
-    'PATCH' => require __DIR__ . '/PATCH.php',
-    'DELETE'=> require __DIR__ . '/DELETE.php'
+    'GET'   => $getUrls('GET'),
+    'POST'  => $getUrls('POST'),
+    'PUT'   => $getUrls('PUT'),
+    'PATCH' => $getUrls('PATCH'),
+    'DELETE'=> $getUrls('DELETE')
 ];
