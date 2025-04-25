03de21e85a26b37452ebb4db688dcd1938a37a2f
Shadow Walker <franklynpeter2006@gmail.com>
2025-04-25 11:24:01 +0100

    update 'mockHTTPRequestTestCases' data provider to return 'requestBuilder' callable to build Request objects; update 'createMockRequest' helper method to populate  and  arrays; add 'getExpectedValue' helper method to get expected values

--- tests/MockHTTPRequestTestCase.php
+++ tests/MockHTTPRequestTestCase.php
@@ -14,22 +14,37 @@ class MockHTTPRequestTestCase extends BaseTestCase
         foreach ($testUrls as $requestMethod => $testUrlTestCases) {
 
             foreach ($testUrlTestCases as $testUrlTestCase) {
-                $testUrlTestCase['method'] = $requestMethod;
 
-                $url = $testUrlTestCase['url'];
-                yield "[$requestMethod] $url" => $testUrlTestCase;
+                $url            = $testUrlTestCase['url'];
+                $data           = $testUrlTestCase['info']['data'] ?? [];
+                $requestBuilder = fn(self $testCase): Request => (
+                    $testCase->createMockRequest($url, $requestMethod, $data)
+                );
+
+                yield "[$requestMethod] $url" => [
+                    'requestBuilder'    => $requestBuilder,
+                    'expected'          => $testUrlTestCase['expected']
+                ];
             }
         }
     }
 
-    public function createMockRequest(string $url, string $method): Request&MockObject
+    public function createMockRequest(string $url, string $method, array $data=[]): Request&MockObject
     {
         // Prepare server variables
         $_SERVER['REQUEST_URI'] = $url;
         $_SERVER['REQUEST_METHOD'] = $method;
 
+        if ($method === 'GET') $_GET = $data;
+        else $_POST = $data;
+
         return $this->getMockBuilder(Request::class)
             ->setConstructorArgs([$_SERVER])
             ->getMock();
+    }
+
+    public static function getExpectedValue(array $expected, string $category, string $key, mixed $default=null): mixed
+    {
+        return $expected[$category][$key] ?? $default;
     }
 }
