2acced2e0d209390c2565f5a40f8aea5545a6bd2
Shadow Walker <franklynpeter2006@gmail.com>
2025-05-09 21:34:41 +0100

    update test controllers to render new views

--- tests/TestControllers/BookController.php
+++ tests/TestControllers/BookController.php
@@ -3,8 +3,31 @@
 namespace PHPAether\Tests\TestControllers;
 
 use PHPAether\Controllers\Controller;
+use PHPAether\Core\HTTP\Request;
+use PHPAether\Core\HTTP\Response;
+use PHPAether\Enums\ResponseStatus;
+use PHPAether\Tests\views\pages\Books;
+use PHPAether\Tests\views\pages\Book;
 
 class BookController extends Controller
 {
 
+    public function index(): Response
+    {
+        $books = [
+            ['id' => 3],
+            ['id' => 8],
+            ['id' => 13]
+        ];
+
+        $props = ['books' => $books];
+        return new Response(ResponseStatus::OK, new Books($props));
+    }
+
+    public function getBook(Request $request): Response
+    {
+        $bookId = $request->getData('id');
+        $props = ['id' => $bookId];
+        return new Response(ResponseStatus::OK, new Book($props));
+    }
 }
