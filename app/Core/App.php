9b52f9d79757524e1ae4abebd7988d963440b9db
Shadow Walker <franklynpeter2006@gmail.com>
2025-05-09 20:59:03 +0100

    add 'resolveRouteAction' method to resolve route action into callables

--- app/Core/App.php
+++ app/Core/App.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace PHPAether\Core;
 
 use PHPAether\Core\HTTP\Request;
+use PHPAether\Core\HTTP\Response;
 use PHPAether\Core\HTTP\Router;
 use PHPAether\Exceptions\Exception;
 use PHPAether\Exceptions\RouterExceptions\MethodNotAllowedException;
@@ -22,9 +23,36 @@ class App
     }
 
     /**
+     * Resolve the route action to a callable
+     * @throws RouterException
+     */
+    public function resolveRouteAction(mixed $action): callable
+    {
+        if (is_callable($action)) {
+            return $action;
+        }
+
+        if (is_array($action) && count($action) === 2)  {
+            [$class_or_object, $method] = $action;
+            if (is_string($class_or_object) && class_exists($class_or_object)) {
+                $instance = new $class_or_object();
+                if (is_callable([$instance, $method])) {
+                    return [$instance, $method];
+                }
+            }
+
+            if (is_callable([$class_or_object, $method])) {
+                return [$class_or_object, $method];
+            }
+        }
+
+        throw new RouterException('Invalid route action');
+    }
+
+    /**
      * @throws Exception
      */
-    public function run(Request $request): string
+    public function run(Request $request): Response|string
     {
 
         try {
@@ -53,10 +81,8 @@ class App
             return "An error occurred";
         }
 
-        // Ensure the action is callable
-        if (! is_callable($action)) {
-            throw new Exception("Invalid request handler. Route action is not callable");
-        }
+        // Resolve the route action
+        $action = $this->resolveRouteAction($action);
 
         // Resolve middlewares
         foreach ($middlewares as $middleware) {
@@ -65,6 +91,6 @@ class App
             $middleware->resolve();
         }
 
-        return $action($request);
+        return call_user_func($action, $request);
     }
 }
