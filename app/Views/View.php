1db7f33d067b8f7c31ec62c2b80358c7a8a65244
Shadow Walker <franklynpeter2006@gmail.com>
2025-05-09 21:17:15 +0100

    full View class redesign - write views as View, Component and Layout classes

--- app/Views/View.php
+++ app/Views/View.php
@@ -1,116 +1,59 @@
 <?php
-
 declare(strict_types=1);
 
 namespace PHPAether\Views;
 
-use PHPAether\Exceptions\FileNotFoundException;
-use PHPAether\Exceptions\NotFoundException\ConfigNotFoundException;
 use PHPAether\Exceptions\ViewException;
-use PHPAether\Utils\Config;
+use PHPAether\Interfaces\RenderableBodyInterface;
 
-class View
+abstract class View extends RenderableBodyInterface
 {
+    private const DEFAULT_DIR = 'pages';
 
-    private const DEFAULT_DIR = "pages";
+    private const ASSETS = [
+        'css'   => [],
+        'js'    => []
+    ];
 
-    protected string $viewFile;
+    public function __construct(
+        public readonly array $props=[]
+    )
+    {
+    }
+
+    abstract function render(): string;
+
+    public function htmlHead(): string
+    {
+        $cssTags = join("\n", array_map(function (string $cssFile): string {
+            return "<link type='text/css' rel='stylesheet' href='$cssFile' />";
+        }, self::ASSETS['css']));
+        $jsTags = join("\n", array_map(function (string $jsFile): string {
+            return "<script type='text/javascript' src='$jsFile' defer></script>";
+        }, self::ASSETS['js']));
+
+        return '';
+    }
+
+    public function toString(): string
+    {
+        return $this->render();
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
 
     /**
-     * @throws FileNotFoundException When the view file specified cannot be found
-     * @throws ViewException When the default view directory is not found in the app config
+     * @throws ViewException
      */
-    public function __construct(string $viewName)
+    public function __get(string $name)
     {
-        $viewFile = ("$defaultDir/" ?? "/app/views/") . static::DEFAULT_DIR . "/$viewName.view.php";
-        try { $defaultViewDir = Config::get('DEFAULT_VIEW_DIR'); }
-        catch (ConfigNotFoundException) {
-            throw new ViewException("Default view directory not found in app configuration");
+        if (empty($this->props[$name])) {
+            throw new ViewException('Invalid prop: ' . $name);
         }
 
-        $viewSubDir = static::DEFAULT_DIR;
-        $viewFile = "{$defaultViewDir}/{$viewSubDir}/{$viewName}.view.php";
-        if (! file_exists($viewFile)) {
-            throw new FileNotFoundException(
-                "The provided view file: $viewFile is not found"
-            );
-        }
-
-        $this->viewFile = $viewFile;
-    }
-
-    protected static function parseVariables(string $template, array $props): string
-    {
-
-        // Matches
-        $abc        = "[a-zA-Z]";
-        $abc_       = "[a-zA-Z_]";
-        $abc123_    = "[a-zA-Z0-9_]";
-        $varName    = "({$abc}{$abc123_}+)";
-
-        $regex = "/\{\{ *$varName(\.$varName)? *\}\}/";
-        $replace_callback = function (array $match) use ($props) {
-            if (count($match) === 4) {
-                [ , $propName, , $propProperty] = $match;
-                $value = $props[$propName][$propProperty] ?? null;
-            } else {
-                [ , $propName] = $match;
-                $value = $props[$propName] ?? null;
-            }
-
-            if ($value === null && false) {
-                throw new ViewException("Undefined prop: {$match[1]}");
-            }
-
-            return $value;
-            return "<?php echo '$value'; ?>";
-        };
-
-        return preg_replace_callback($regex, $replace_callback, $template);
-    }
-
-    protected static function parseForLoops(string $template, array $props): string
-    {
-
-        // Matches
-        $abc        = "[a-zA-Z]";
-        $abc_       = "[a-zA-Z_]";
-        $abc123_    = "[a-zA-Z0-9_]";
-        $varName    = "({$abc}{$abc123_}*)";
-
-        $regex = "/@for *\(($varName:)? $varName of $varName*\) \{.+\}/";
-        $regex = "/@for *\(($varName\:)?/";
-        $replace_callback = function (array $match) use ($props) {
-            var_dump($match);
-            if (count($match) === 4) {
-                [ , $propName, , $propProperty] = $match;
-                $value = $props[$propName][$propProperty] ?? null   ;
-            } else {
-                [ , $propName] = $match;
-                $value = $props[$propName] ?? null;
-            }
-
-            if ($value === null && false) {
-                throw new ViewException("Undefined prop: {$match[1]}");
-            }
-
-            return $value;
-            return "<?php echo '$value'; ?>";
-        };
-
-        return preg_replace_callback($regex, $replace_callback, $template);
-
-    }
-
-    public function render(array $props=[]): string
-    {
-        // Get the content of the file
-        $view = file_get_contents($this->viewFile);
-
-        // Extract props
-        $view = static::parseForLoops($view, $props);
-        // $view = static::parseVariables($view, $props);
-
-        return $view;
+        return $this->props[$name];
     }
 }
